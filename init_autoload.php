f721649ec79bb6c38cfcefe864b0723fec0b6fef
Adjy Sedar Desir <desiradjysedar@gmail.com>
2023-03-07 23:47:07 +0100

    Delete init_autoload.php

--- init_autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-function autoload($class_name){
-    // on remplace les \ du namespace par les / des répertoires
-    $class_name = str_replace('\\', '/', $class_name) ;
-
-    // on inclut le fichier correspondant
-    require $class_name . '.php' ;
-}
-spl_autoload_register('autoload') ;
